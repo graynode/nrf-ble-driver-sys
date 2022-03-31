8c6afbb6dfb49ebdce3e562a352b2ebd1ebfae32
matt <matt@zonkey.none>
2022-03-30 21:50:01 -0400

    Added windows build support

--- build.rs
+++ build.rs
@@ -1,6 +1,18 @@
 extern crate vcpkg;
-use cmake;
+use cmake::Config;
 use std::env;
+
+const NRF_BLE_RELEASE_VERSION: &str = "4.1.4";
+#[cfg(any(target_os = "linux", target_os = "macos"))]
+const NRF_BLE_DRIVER_LIB_NAME: &str = "nrf-ble-driver-sd_api_v6";
+#[cfg(target_os = "windows")]
+#[cfg(not(debug_assertions))]
+const NRF_BLE_DRIVER_LIB_NAME: &str = "nrf-ble-driver-sd_api_v6-mt-static-4_1_4";
+#[cfg(target_os = "windows")]
+#[cfg(debug_assertions)]
+const NRF_BLE_DRIVER_LIB_NAME: &str = "nrf-ble-driver-sd_api_v6-mt-static-gd-4_1_4";
+
+
 
 fn find_dependencies() -> bool {
     env::var("VCPKG_ROOT").unwrap();
@@ -25,11 +37,14 @@ fn find_dependencies() -> bool {
 }
 
 fn build_sources() -> bool {
-    let dest = cmake::build("src/pc-ble-driver");
+    let dest = Config::new("src/pc-ble-driver")
+    .define("NRF_BLE_DRIVER_VERSION", NRF_BLE_RELEASE_VERSION)
+    .build();
+
     let lib_folder = dest.join("lib");
     println!("cargo:warning=dst: {}", dest.display());
     println!("cargo:rustc-link-search=native={}", lib_folder.display());
-    println!("cargo:rustc-link-lib=nrf-ble-driver-sd_api_v6");
+    println!("cargo:rustc-link-lib=static={}", NRF_BLE_DRIVER_LIB_NAME);
     return true;
 
 }
